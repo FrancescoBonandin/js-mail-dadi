9769e18fcef4945fbe3ca66170160bd25ea85b92
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-06-15 16:35:58 +0200

    Bug ricarica troppo velocemente

--- Dadi/js/my-script.js
+++ Dadi/js/my-script.js
@@ -7,3 +7,55 @@
 //      -SE A > B vince a -> stampo the winner is a
 //      -ALTRIMENTI SE a A < B stampo the winner is b
 //      -ALTRIMENTI a = b stampo TIE
+
+const form = document.getElementById("form")
+
+form.addEventListener("submit", function(event){
+   
+    event.preventDefault;
+
+    const playerInput = document.getElementById("player");
+
+    const cpuinput = document.getElementById("cpu");
+
+    const result = document.getElementById("result");
+
+    result.innerHTML = "";
+
+    let i;
+
+    for( i=0; i < 1; i++){
+        
+        
+        let player = Math.floor( Math.random()*6 ) + 1;
+        console.log(player)
+        
+        let cpu =  Math.floor( Math.random()*6 ) + 1;
+           console.log(cpu)
+ 
+
+            if(player > cpu){
+
+                result.append("Player Wins")
+
+            }
+
+            else if(player < cpu){
+
+                result.append("CPU Wins")
+
+            }
+
+            else{
+
+                result.append("IT'S A TIE")
+
+            }
+
+            playerInput.value = player;
+            cpuinput.value = cpu;
+            
+    }
+}
+)
+
